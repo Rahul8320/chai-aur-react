fbbb38cd86f0d0607259bc254ad20cc84e03cdaa
Rahul Dey <rahuldey832000@gmail.com>
2023-10-16 21:24:20 +0530

    Add custom react v1

---
+++ CustomReact/main.js
@@ -0,0 +1,22 @@
+function customRender(element, container) {
+  const domElement = document.createElement(element.type);
+  domElement.innerHTML = element.children;
+
+  for (const [key, value] of Object.entries(element.props)) {
+    domElement.setAttribute(key, value);
+  }
+
+  container.appendChild(domElement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://youtube.com/@chaiaurcode",
+    target: "_blank",
+  },
+  children: "Click here to visit chai aur code",
+};
+
+const mainContainer = document.getElementById("root");
+customRender(reactElement, mainContainer);
