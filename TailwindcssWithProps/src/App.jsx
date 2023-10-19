4683457ae0174d477001c448accf0e3eddd332ce
Rahul Dey <rahuldey832000@gmail.com>
2023-10-19 22:31:23 +0530

    Add Card component

--- TailwindcssWithProps/src/App.jsx
+++ TailwindcssWithProps/src/App.jsx
@@ -1,10 +1,15 @@
-import "./App.css";
+import { Card } from "./Card";
 
 function App() {
   return (
-    <>
-      <h1 className="text-center text-blue-500">Tailwind css test</h1>
-    </>
+    <div className="container mx-auto w-1/3 mt-32 max-h-screen">
+      <h1 className="text-center text-black rounded-xl p-4 bg-green-400 mb-16">
+        Tailwind css test
+      </h1>
+
+      <Card name="Macbook" description="M2 mac pro" />
+      <Card name="Dell G15" description="Windows 11" />
+    </div>
   );
 }
 
