c56b774f19271dc338aaf62165adb1ba46d058f3
Rahul Dey <rahuldey832000@gmail.com>
2023-10-20 13:24:29 +0530

    add copy to clipboard functionality

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,10 +1,15 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { TbClipboardText } from "react-icons/tb";
+import { BsArrowRepeat } from "react-icons/bs";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  // useRef hook
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let newPassword = "";
@@ -21,9 +26,14 @@ function App() {
     setPassword(newPassword);
   }, [length, numberAllowed, charAllowed, setPassword]);
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
   useEffect(() => {
     passwordGenerator();
-  }, [passwordGenerator]);
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
 
   return (
     <div className="w-full max-w-4xl mx-auto shadow-md rounded-lg px-4 py-6 mt-20 text-orange-500 bg-slate-700">
@@ -34,10 +44,17 @@ function App() {
           className="outline-none w-full py-1 px-3 text-2xl font-mono"
           value={password}
           placeholder="password"
+          ref={passwordRef}
           readOnly
         />
-        <button className="px-3 py-0.5 shrink-0 outline-none bg-blue-700 text-lg text-white">
-          Copy
+        <button
+          onClick={copyPasswordToClipboard}
+          className="px-3 py-0.5 shrink-0 outline-none bg-blue-700 text-lg text-white hover:shadow-lg hover:bg-blue-800"
+        >
+          <span className="flex items-center gap-x-1">
+            <TbClipboardText />
+            Copy
+          </span>
         </button>
       </div>
       <div className="flex text-md font-medium gap-x-7 mx-16">
@@ -76,7 +93,10 @@ function App() {
             className="outline-none cursor-pointer hover:text-indigo-500 hover:underline hover:underline-offset-2 decoration-orange-500"
             onClick={passwordGenerator}
           >
-            Generate New
+            <span className="flex items-center gap-x-1">
+              <BsArrowRepeat />
+              Generate New
+            </span>
           </button>
         </div>
       </div>
