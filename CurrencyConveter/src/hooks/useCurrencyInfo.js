3f6bc5316de89da3d1d39d4f7554532a46fc1b9a
Rahul Dey <rahuldey832000@gmail.com>
2023-10-21 19:21:16 +0530

    Update Currency converter

--- CurrencyConveter/src/hooks/useCurrencyInfo.js
+++ CurrencyConveter/src/hooks/useCurrencyInfo.js
@@ -3,14 +3,13 @@ import { useState, useEffect } from "react";
 const useCurrencyInfo = (currency) => {
   const [data, setData] = useState({});
 
-  const url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`;
-
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json)
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
       .then((res) => setData(res[currency]));
-    console.log(data);
-  }, [currency, url, data]);
+  }, [currency]);
 
   return data;
 };
