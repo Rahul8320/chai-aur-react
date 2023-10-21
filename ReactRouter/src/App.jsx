b61f851efdb0ffa1d30a4e0205115664b6d6f878
Rahul Dey <rahuldey832000@gmail.com>
2023-10-21 19:37:00 +0530

    Add tailwind css

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>React Router Project</h1>
+      <h1 className="bg-green-300 text-4xl text-center">
+        React Router Project
+      </h1>
     </>
   );
 }
