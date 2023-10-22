1dbbf8f01bce0172f062f5f0df04b181dd01df12
Rahul Dey <rahuldey832000@gmail.com>
2023-10-22 13:47:30 +0530

    Add USer Context setup

--- ReactContextApi/src/App.jsx
+++ ReactContextApi/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
       <h1>React context api</h1>
-    </>
+    </UserContextProvider>
   );
 }
 
