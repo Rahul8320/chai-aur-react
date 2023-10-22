08edf493be15c8b9bdae42b5bcae20cad51b39bc
Rahul Dey <rahuldey832000@gmail.com>
2023-10-22 14:25:11 +0530

    add theme context

--- ThemeSwitcher/src/App.jsx
+++ ThemeSwitcher/src/App.jsx
@@ -2,9 +2,15 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="p-4 bg-pink-500 text-3xl">Pink city</h1>
-    </>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* theme button */}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">{/* card component */}</div>
+      </div>
+    </div>
   );
 }
 
