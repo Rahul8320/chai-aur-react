2dd664ab1257ba43f998df90a73495711e364b16
Rahul Dey <rahuldey832000@gmail.com>
2023-10-23 19:21:07 +0530

    Add local storage

--- TodoContextApi/src/App.jsx
+++ TodoContextApi/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoProvider } from "./contexts/TodoContext";
 
 function App() {
@@ -23,6 +23,18 @@ function App() {
       )
     );
   };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <TodoProvider
