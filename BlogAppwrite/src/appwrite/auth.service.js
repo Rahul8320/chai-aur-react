ec5a056de229e090ac27008e8e8e2603fafddee0
Rahul Dey <rahuldey832000@gmail.com>
2023-10-28 23:36:49 +0530

    create auth service

---
+++ BlogAppwrite/src/appwrite/auth.service.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-useless-catch */
+import appwriteConfig from "../config/appwrite.config";
+import { Client, Account, ID } from "appwrite";
+
+class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(appwriteConfig.Url)
+      .setProject(appwriteConfig.ProjectId);
+    this.account = new Account(this.client);
+  }
+
+  // create new user account
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // call login method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // login to user account
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // get current login user account
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // logout user account
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
