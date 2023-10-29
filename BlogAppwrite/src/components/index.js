531871ab5620a531738281e9cbba4551b2eeacba
Rahul Dey <rahuldey832000@gmail.com>
2023-10-29 22:30:58 +0530

    create select component

--- BlogAppwrite/src/components/index.js
+++ BlogAppwrite/src/components/index.js
@@ -3,5 +3,8 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./footer/Logo";
 import LogoutBtn from "./header/LogoutBtn";
+import Button from "./common/Button";
+import Input from "./common/Input";
+import Select from "./common/Select";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select };
