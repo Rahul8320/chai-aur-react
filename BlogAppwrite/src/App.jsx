eef970cbc762023cc0ffafbbe044459a0c165744
Rahul Dey <rahuldey832000@gmail.com>
2023-10-29 19:10:20 +0530

    add tailwind css

--- BlogAppwrite/src/App.jsx
+++ BlogAppwrite/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>A blog app with appwrite</h1>
+      <h1 className="text-3xl font-bold text-cyan-200">
+        A blog app with appwrite
+      </h1>
     </>
   );
 }
