b3733419843e463da6c6a34743859b069fcb32dd
Rahul Dey <rahuldey832000@gmail.com>
2023-11-03 21:16:01 +0530

    Create router

--- BlogAppwrite/src/main.jsx
+++ BlogAppwrite/src/main.jsx
@@ -4,11 +4,79 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  AddPostPage,
+  AllPostsPage,
+  EditPostPage,
+  HomePage,
+  LoginPage,
+  PostPage,
+  RegisterPage,
+} from "./pages/index.js";
+import { Protected } from "./components/index.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: (
+          <Protected authentication={false}>
+            <LoginPage />
+          </Protected>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <Protected authentication={false}>
+            <RegisterPage />
+          </Protected>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <Protected authentication>
+            <AllPostsPage />
+          </Protected>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <Protected authentication>
+            <AddPostPage />
+          </Protected>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <Protected authentication>
+            <EditPostPage />
+          </Protected>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <PostPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
